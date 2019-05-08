b22012fa32377a5bb75a32b02d42a1bb0ddc442e
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-05-08 14:07:57 +0200

    Mise en place d'un main_container et de la flexBox

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Search/>
+
     );
   }
 }
--- Components/Search.js
+++ Components/Search.js
@@ -1,18 +1,36 @@
 import React from 'react'
-import {View, TextInput, Button} from 'react-native'
+import {View, TextInput, Button, StyleSheet} from 'react-native'
 
-class Search extends React.Component
-{
+class Search extends React.Component {
     render() {
         return (
-            <View>
-                <TextInput placeholder="Titre du film"/>
-                <Button title="Rechercher" onPress={function() {}} />
+            <View style={styles.main_container}>
+                <TextInput style={styles.textinput} placeholder="Titre du film"/>
+                <Button title="Rechercher" onPress={function () {
+                }}/>
             </View>
 
         )
 
     }
 }
+
+
+const styles = StyleSheet.create({
+    main_container:{
+        marginTop:25,
+        flex:1
+    },
+    textinput: {
+
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+    }
+})
+
 
 export default Search
