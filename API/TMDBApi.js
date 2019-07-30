db3b907fc3089088382e095d5e1573e8745b4317
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-07-30 12:02:01 +0200

    Mise en place du scroll infini et de la requête asynchrone

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,7 +1,8 @@
 const API_TOKEN="abee8f7e52feda7ef51131dd9a2b75da"
 
-export function getFilmsFromApiWithSearchedText (text) {
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text +
+        '&page=' + page
     return fetch (url)
         .then((response) => response.json())
         .catch((error) => console.log(error))
--- Components/Search.js
+++ Components/Search.js
@@ -1,33 +1,52 @@
+// Components/Search.js
+
 import React from 'react'
-import {View, TextInput, Button, StyleSheet, FlatList, Text, ActivityIndicator} from 'react-native'
-import films from '../Helpers/filmsData'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
-import {getFilmsFromApiWithSearchedText} from '../API/TMDBApi'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
 
-    constructor(props)
-    {
+    constructor(props) {
         super(props)
+        this.searchedText = ""
+        this.page = 0
+        this.totalPages = 0
         this.state = {
             films: [],
             isLoading: false
         }
-        this.searchedText =""
     }
-    _loadFilms()
-    {
-        this.setState({isLoading: true})
+
+    _loadFilms() {
         if (this.searchedText.length > 0) {
-            getFilmsFromApiWithSearchedText(this.searchedText).then(data => this.setState({
-                films : data.results,
-                isLoading: false
-            }))
+            this.setState({ isLoading: true })
+            getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+                this.page = data.page
+                this.totalPages = data.total_pages
+                this.setState({
+                    films: [ ...this.state.films, ...data.results ],
+                    isLoading: false
+                })
+            })
         }
     }
 
-    _displayLoading()
-    {
+    _searchTextInputChanged(text) {
+        this.searchedText = text
+    }
+
+    _searchFilms() {
+        this.page = 0
+        this.totalPages = 0
+        this.setState({
+            films: [],
+        }, () => {
+            this._loadFilms()
+        })
+    }
+
+    _displayLoading() {
         if (this.state.isLoading) {
             return (
                 <View style={styles.loading_container}>
@@ -37,34 +56,37 @@ class Search extends React.Component {
         }
     }
 
-    _searchTextInputChanged(text)
-    {
-        this.searchedText = text
-    }
-
     render() {
         return (
             <View style={styles.main_container}>
-                <TextInput onSubmitEditing={() =>this._loadFilms()} onChangeText={(text) => this._searchTextInputChanged(text)} style={styles.textinput} placeholder="Titre du film"/>
-                <Button title="Rechercher" onPress={ () => {this._loadFilms()}}/>
+                <TextInput
+                    style={styles.textinput}
+                    placeholder='Titre du film'
+                    onChangeText={(text) => this._searchTextInputChanged(text)}
+                    onSubmitEditing={() => this._searchFilms()}
+                />
+                <Button title='Rechercher' onPress={() => this._searchFilms()}/>
                 <FlatList
                     data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <FilmItem film={item}/>}
+                    onEndReachedThreshold={0.5}
+                    onEndReached={() => {
+                        if (this.page < this.totalPages) {
+                            this._loadFilms()
+                        }
+                    }}
                 />
                 {this._displayLoading()}
             </View>
-
         )
-
     }
 }
 
-
 const styles = StyleSheet.create({
-    main_container:{
-        marginTop:25,
-        flex:1
+    main_container: {
+        flex: 1,
+        marginTop: 20
     },
     textinput: {
         marginLeft: 5,
