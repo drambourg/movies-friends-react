2a14e6d0af863420cf4ea6270df19741c4aa9823
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-07-31 14:16:53 +0200

    Extenalisation du FadeIn Pour les FilmItem

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,8 +1,9 @@
 // Components/FilmItem.js
 
 import React from 'react'
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
+import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native'
 import {getImageFromAPI} from "../API/TMDBApi";
+import FadeIn from "../Animations/FadeIn";
 
 class FilmItem extends React.Component {
 
@@ -20,26 +21,28 @@ class FilmItem extends React.Component {
     render() {
         const {film, displayDetailForFilm} = this.props
         return (
-            <TouchableOpacity style={styles.main_container}
-            onPress={() => displayDetailForFilm(film.id)}>
-                <Image
-                    style={styles.image}
-                    source={{uri: getImageFromAPI(film.poster_path)}}
-                />
-                <View style={styles.content_container}>
-                    <View style={styles.header_container}>
-                        {this._displayFavoriteImage()}
-                        <Text style={styles.title_text}>{film.title}</Text>
-                        <Text style={styles.vote_text}>{film.vote_average}</Text>
+            <FadeIn>
+                <TouchableOpacity style={styles.main_container}
+                                  onPress={() => displayDetailForFilm(film.id)}>
+                    <Image
+                        style={styles.image}
+                        source={{uri: getImageFromAPI(film.poster_path)}}
+                    />
+                    <View style={styles.content_container}>
+                        <View style={styles.header_container}>
+                            {this._displayFavoriteImage()}
+                            <Text style={styles.title_text}>{film.title}</Text>
+                            <Text style={styles.vote_text}>{film.vote_average}</Text>
+                        </View>
+                        <View style={styles.description_container}>
+                            <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
+                        </View>
+                        <View style={styles.date_container}>
+                            <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
+                        </View>
                     </View>
-                    <View style={styles.description_container}>
-                        <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
-                    </View>
-                    <View style={styles.date_container}>
-                        <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
+                </TouchableOpacity>
+            </FadeIn>
         )
     }
 }
--- Components/Test.js
+++ Components/Test.js
@@ -1,74 +1,38 @@
 // Components/Test.js
 
 import React from 'react'
-import {StyleSheet, View, Animated, Easing} from 'react-native'
+import {StyleSheet, View, Animated, Easing, PanResponder, Dimensions} from 'react-native'
 
 class Test extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            topPosition: new Animated.Value(0),
-            leftPosition: new Animated.Value(0),
+            topPosition: 0,
+            leftPosition: 0,
         }
-    }
 
-    componentDidMount() {
-        Animated.timing(
-            this.state.topPosition, {
-                toValue: 100,
-                duration: 3000,
-                easing: Easing.bounce
-            }
-        ).start()
-/*        Animated.spring(
-            this.state.topPosition,
-            {
-                toValue: 100,
-                speed: 4,
-                bounciness: 30
-            }
-        ).start();*/
-
-/*        Animated.decay(
-            this.state.topPosition,
-            {
-                velocity: 0.8,
-                deceleration: 0.997,
-            }
-        ).start();*/
-
-/*        Animated.parallel([
-            Animated.spring(
-                this.state.topPosition,
-                {
-                    toValue: 100,
-                    tension: 8,
-                    friction: 3
+        var {height, width} = Dimensions.get('window');
+        this.panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onPanResponderMove: (evt, gestureState) => {
+                let touches = evt.nativeEvent.touches;
+                if (touches.length == 1) {
+                    this.setState({
+                        topPosition: touches[0].pageY - height/2,
+                        leftPosition: touches[0].pageX - width/2
+                    })
                 }
-            ),
-            Animated.timing(
-                this.state.leftPosition,
-                {
-                    toValue: 100,
-                    duration: 1000,
-                    easing: Easing.elastic(2)
-                }
-            )
-        ]).start()*/
+            }
+        })
     }
 
     render() {
         return (
             <View style={styles.main_container}>
-                <Animated.View
-                    style={[
-                        styles.animation_view,
-                        {
-                            top: this.state.topPosition,
-                            left: this.state.leftPosition
-                        }]
-                    }>
-                </Animated.View>
+                <View
+                    {...this.panResponder.panHandlers}
+                    style={[styles.animation_view, { top: this.state.topPosition, left: this.state.leftPosition }]}>
+                </View>
             </View>
         )
     }
