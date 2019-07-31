df59c81105a393ec0b9db6b7117f431854aea070
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-07-31 11:51:25 +0200

    Premiers essais d animations

--- Components/Test.js
+++ Components/Test.js
@@ -1,15 +1,74 @@
 // Components/Test.js
 
 import React from 'react'
-import { StyleSheet, View, Platform } from 'react-native'
+import {StyleSheet, View, Animated, Easing} from 'react-native'
 
 class Test extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            topPosition: new Animated.Value(0),
+            leftPosition: new Animated.Value(0),
+        }
+    }
+
+    componentDidMount() {
+        Animated.timing(
+            this.state.topPosition, {
+                toValue: 100,
+                duration: 3000,
+                easing: Easing.bounce
+            }
+        ).start()
+/*        Animated.spring(
+            this.state.topPosition,
+            {
+                toValue: 100,
+                speed: 4,
+                bounciness: 30
+            }
+        ).start();*/
+
+/*        Animated.decay(
+            this.state.topPosition,
+            {
+                velocity: 0.8,
+                deceleration: 0.997,
+            }
+        ).start();*/
+
+/*        Animated.parallel([
+            Animated.spring(
+                this.state.topPosition,
+                {
+                    toValue: 100,
+                    tension: 8,
+                    friction: 3
+                }
+            ),
+            Animated.timing(
+                this.state.leftPosition,
+                {
+                    toValue: 100,
+                    duration: 1000,
+                    easing: Easing.elastic(2)
+                }
+            )
+        ]).start()*/
+    }
 
     render() {
         return (
             <View style={styles.main_container}>
-                <View style={styles.subview_container}>
-                </View>
+                <Animated.View
+                    style={[
+                        styles.animation_view,
+                        {
+                            top: this.state.topPosition,
+                            left: this.state.leftPosition
+                        }]
+                    }>
+                </Animated.View>
             </View>
         )
     }
@@ -21,20 +80,10 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-
-    subview_container: {
-        ...Platform.select({
-            ios: {
-                backgroundColor: 'red',
-                height: 100,
-                width: 50
-            },
-            android: {
-                backgroundColor: 'blue',
-                height: 50,
-                width: 100
-            }
-        })
+    animation_view: {
+        backgroundColor: 'red',
+        width: 100,
+        height: 100
     }
 })
 
